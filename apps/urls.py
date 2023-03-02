7e79623859228abd024e706321d3832ac4db297c
nahK994 <nkskl6@gmail.com>
2023-03-02 13:17:03 +0600

    add swagger API documentation

--- apps/urls.py
+++ apps/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from apps.company_management_app import views as company_views
+from rest_framework.routers import DefaultRouter
+from rest_framework_swagger.views import get_swagger_view
+from django.conf.urls import url
+
+
+router = DefaultRouter()
+
+base_company_url = "api/company"
+router.register(base_company_url, company_views.CompanyTypeViewset, basename="company")
+
+schema_view = get_swagger_view(title='Config Management Services API', patterns=router.urls)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('companies', include('apps.company_management_app.urls')),
+    path(base_company_url+'/', include('apps.company_management_app.urls')),
+    url('api/docs', schema_view)
 ]
